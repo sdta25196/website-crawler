9b3e59e2039d43d682d911357bc17b7423a4b719
田源 <tianyuan@eol.cn>
2023-05-05 19:02:24 +0800

    feat: 修改301死循环的问题

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Crawler from "crawler"
-import { handleHref, log, writeFile } from "./tools.js"
+import { handleHref, log, transformEnds, transformHTTP, writeFile } from "./tools.js"
 
 /**
 *
@@ -14,53 +14,61 @@ function run({ mainHost, startHost, recordLowHost = false }) {
   const crawledHrefsQueue = new Set() // 爬过的链接
   const waitHrefsQueue = new Set() // 等待爬取的链接
   const lowHost = new Set()  // 二级域名
-  let tempHref = ''
+  let tempHref = startHost
   const folderName = startHost.replace(/https?:\/\//, '').replace(/\//g, '') // 文件夹名称
 
   var crawler = new Crawler({
-    maxConnections: 3,
+    maxConnections: 1,
     // 在每个请求处理完毕后将调用此回调函数
     callback: function (error, res, done) {
       if (error || res.statusCode > 400) {
         // ! 出错直接下一个
+        log("错误：：：当前等待队列中拥有:", waitHrefsQueue.size)
         const [nextHrefs] = waitHrefsQueue // 取出集合里下一个需要爬的href
         waitHrefsQueue.delete(nextHrefs) // 删除
         crawler.queue(nextHrefs)
-        log("错误：：：当前等待队列中拥有:", waitHrefsQueue.size, "\n下一个抓：", nextHrefs)
+        log("下一个抓：", nextHrefs)
         writeFile(folderName, 'err.txt', (res.request?.uri?.href || (tempHref + '::状态::' + res.statusCode)) + '\n')
       } else {
         var $ = res.$
         if ($) {
           writeFile(folderName, 'hrefs.txt', (res.request?.uri?.href || (tempHref + '::状态::' + res.statusCode)) + '\n')
           const currentOrigin = res.request.uri.protocol + '//' + res.request.uri.host // 当前域名
-          const currentHref = res.request.uri.href// 当前域名
+          const currentHref = res.request.uri.href // 当前url完整地址
           crawledHrefsQueue.add(currentHref) // 抓完一个之后，存到已经抓取的set里
           // 它是核心jQuery的精简实现，可以按照jQuery选择器语法快速提取DOM元素
           $("a").each(function (_, a) {
             const href = $(a).attr('href')
-            const finalyHref = handleHref(href, currentOrigin, mainHost)
+            const finalyHref = handleHref(href, currentHref, mainHost)
             if (finalyHref) {
               // ! 二级域名要单独存起来
-              if (recordLowHost && !finalyHref.startsWith(currentOrigin)) {
+              let curl = new URL(currentOrigin)
+              let furl = new URL(finalyHref)
+              if (recordLowHost && curl.hostname !== furl.hostname) {
                 lowHost.add(finalyHref)
               } else {
-                // ! 已经爬过的href里没有才加入等待集合。
-                if (!crawledHrefsQueue.has(finalyHref)) {
+                // ! 已经爬过的href里没有才加入等待集合。http 和 https 算一个, 结尾带反斜杠和不带的算一个
+                if (!crawledHrefsQueue.has(finalyHref) &&
+                  !crawledHrefsQueue.has(transformHTTP(finalyHref)) &&
+                  !crawledHrefsQueue.has(transformHTTP(transformEnds(finalyHref))) &&
+                  !crawledHrefsQueue.has(transformEnds(transformHTTP(finalyHref))) &&
+                  !crawledHrefsQueue.has(transformEnds(finalyHref))) {
                   waitHrefsQueue.add(finalyHref)
                 }
               }
             }
           })
         }
+        log("当前等待队列中拥有:", waitHrefsQueue.size)
         const [nextHrefs] = waitHrefsQueue // 取出集合里下一个需要爬的href
         waitHrefsQueue.delete(nextHrefs) // 删除
         crawler.queue(nextHrefs)
         tempHref = nextHrefs // 临时记录请求地址。
-        log("当前等待队列中拥有:", waitHrefsQueue.size, "\n下一个抓：", nextHrefs)
+        log("下一个抓：", nextHrefs)
       }
       // 输出一下二级页面
       if (recordLowHost && waitHrefsQueue.size === 1) {
-        writeFile(folderName, 'lowHost.txt', '\n\n\n' + [...lowHost].toString() + '\n',)
+        writeFile(folderName, 'lowHost.txt', [...lowHost].join('\n'))
       }
       done()
     }
@@ -73,6 +81,9 @@ function run({ mainHost, startHost, recordLowHost = false }) {
 // ! 菏泽学院  https://www.hezeu.edu.cn/        26万收录
 // ! 泰安一中  https://www.tadyz.com/          3万收录
 run({
+  // mainHost: 'wyu.edu.cn',
+  // startHost: 'https://www.wyu.edu.cn/',
   mainHost: 'hezeu.edu.cn',
   startHost: 'https://www.hezeu.edu.cn/',
+  recordLowHost: true
 })
--- src/tools.js
+++ src/tools.js
@@ -20,20 +20,39 @@ export function log(...str) {
   process.stdout.write(str.join('') + '\n')
 }
 
-export function handleHref(href, currentOrigin, mainHost) {
+/** 转换http与https */
+export function transformHTTP(href) {
+  if (href.startsWith('http://')) return href.replace('http://', 'https://')
+  if (href.startsWith('https://')) return href.replace('https://', 'http://')
+  return href
+}
+
+/** 转换结尾斜杠 */
+export function transformEnds(href) {
+  if (href.endsWith('/')) return href.replace(/\/$/, '')
+  return href + '/'
+}
+
+export function handleHref(href, currentHref, mainHost) {
 
   // ! 非法链接不要
   if (!href) return ''
+
   // ! 链接 javascript 开头的不要
   if (href.startsWith('javascript')) return ''
+
   // ! 文件链接不要 
   if (href.endsWith('.zip') || href.endsWith('.doc') || href.endsWith('.png') || href.endsWith('.jpg')) return ''
 
+  // ! 锚点不要
+  if (href.indexOf('#') !== -1) {
+    href = href.split('#')[0]
+  }
   // ! 邮箱跳转不要
   if (href.indexOf('mailto:') !== -1) return ''
 
-  if (href.startsWith('.')) {
-    href = absolutify(currentOrigin, href)
+  if (!href.startsWith('http')) {
+    href = absolutify(currentHref, href)
   }
 
   // ! 非本站主域名链接不要
